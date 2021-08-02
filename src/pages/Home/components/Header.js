2df3a090129dba8e984764331f4334a203ee5036
Brennogsn <brennogabriel12@live.com>
2021-08-02 19:54:40 -0300

    Initial Code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className='header'> 
+          <div className='toolbar'>
+            <div>
+              <a href='/'>Inicio</a>
+            </div>
+            <div>
+              <button>New post</button>
+              <span>Img1</span>
+              <span>Img2 - alteração</span>
+            </div>
+          </div>  
+        </header>
+    )
+}
+
+export default Header;
